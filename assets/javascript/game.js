d4d8859842c47c731109d8c0787723992b8bfad0
Jamie Barton <jbarton10@email.arizona.edu>
2019-05-29 13:24:27 -0700

    More changes made to style, and win lose function added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,76 @@
-var total= 0;
+var total = 0;
 console.log(total);
-var goal=Math.floor(Math.random()*(120-19)+19);
-var num1 =Math.floor(Math.random()*13);
-var num2 =Math.floor(Math.random()*13);
-var num3 =Math.floor(Math.random()*13);
-var num4 =Math.floor(Math.random()*13);
-var wins=0;
-var losses=0;
+var goal = Math.floor(Math.random() * (120 - 19) + 19);
+var num1 = Math.floor(Math.random() * 13);
+var num2 = Math.floor(Math.random() * 13);
+var num3 = Math.floor(Math.random() * 13);
+var num4 = Math.floor(Math.random() * 13);
+var wins = 0;
+var losses = 0;
 
-$(document).ready(function(){
+$(document).ready(function () {
     $("#playerTotal").text(total);
     $("#playerGoal").text(goal);
-    
-    //This might have to be done in another function, might only run when the page opens initially.
-    if(total===goal){
-        wins++;
+    $("#winOrLose").text("");
+    $("#winner").text("Wins: "+wins);
 
-    }
-    console.log(goal);
-    console.log(num1);
-    console.log(num2);
-    console.log(num3);
-    console.log(num4);
-    $("#firstRandom").on("click", function(){
-        total+=num1;
+    $("#firstRandom").on("click", function () {
+        total += num1;
         $("#playerTotal").text(total);
-        //Make function to check if player has won or lost
+        winLose();
     });
-    $("#secondRandom").on("click", function(){
-        total+=num2;
+    $("#secondRandom").on("click", function () {
+        total += num2;
         $("#playerTotal").text(total);
-        
+        winLose();
+
     });
-    $("#thirdRandom").on("click", function(){
-        total+=num3;
+    $("#thirdRandom").on("click", function () {
+        total += num3;
         $("#playerTotal").text(total);
-        
+        winLose();
+
     });
-    $("#forthRandom").on("click", function(){
-        total+=num4;
+    $("#forthRandom").on("click", function () {
+        total += num4;
         $("#playerTotal").text(total);
-        
+        winLose();
+
     });
 
-    
+
+
+
 
 });
+
+function winLose() {
+    if (goal === total) {
+        wins++;
+        $("#winOrLose").text("You win!!");
+        total = 0;
+        goal = Math.floor(Math.random() * (120 - 19) + 19);
+        num1 = Math.floor(Math.random() * 13);
+        num2 = Math.floor(Math.random() * 13);
+        num3 = Math.floor(Math.random() * 13);
+        num4 = Math.floor(Math.random() * 13);
+        $("#playerGoal").text(goal);
+        $("#playerTotal").text(total);
+
+
+    }
+    else if(total>goal){
+        losses++;
+        $("#winOrLose").text("You lose :(");
+        total = 0;
+        goal = Math.floor(Math.random() * (120 - 19) + 19);
+        num1 = Math.floor(Math.random() * 13);
+        num2 = Math.floor(Math.random() * 13);
+        num3 = Math.floor(Math.random() * 13);
+        num4 = Math.floor(Math.random() * 13);
+        $("#playerGoal").text(goal);
+        $("#playerTotal").text(total);
+
+    }
+}
 
