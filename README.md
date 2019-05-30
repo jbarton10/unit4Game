0337417a27f7bb8976489a5bf316beed5e32352b
Jamie Barton <jbarton10@email.arizona.edu>
2019-05-29 18:25:04 -0700

    ITS WORKING, style should be added

---
+++ README.md
@@ -0,0 +1,11 @@
+GUESSING GAME
+
+DESCRIPTION:
+This app is an in browser game designed to test your memory and basic arithmitic skills all at once.  It does all this
+while also letting you look into some of my favorite things to do!  There are 4 pictures, one of chess pieces, piano and so on.  When you click on each of these photos they will add a random ammount to your total.  Your goal is to get your random total to match your goal that has been randomly generated.  All of the photos will have the same value until you either win or lose the game. Once the game is over it will display a winning or a losing message.  At this point youre wins and losses will be incremented and the game will start all over!
+
+PURPOSE:
+This project is designed to help practice the use of the library JQuery.  Using elements like $("#idName) to get the elements id rather than document.getElementById ect.
+
+ABOUT THE DESIGNER:
+This project was made by Jamie Barton for the coding bootcamp at the University of Arizona
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,25 @@
+//Main Function
 var total = 0;
 console.log(total);
-var goal = Math.floor(Math.random() * (120 - 19) + 19);
-var num1 = Math.floor(Math.random() * 13);
-var num2 = Math.floor(Math.random() * 13);
-var num3 = Math.floor(Math.random() * 13);
-var num4 = Math.floor(Math.random() * 13);
+var goal = Math.floor(Math.random() * 120) + 19;
+//BUG WHERE IT CHOOSES 0
+var num1 = Math.floor(Math.random() * 12)+1;
+var num2 = Math.floor(Math.random() * 12)+1;
+var num3 = Math.floor(Math.random() * 12)+1;
+var num4 = Math.floor(Math.random() * 12)+1;
 var wins = 0;
 var losses = 0;
 
+//Main Function
 $(document).ready(function () {
+    //Writing to the page
     $("#playerTotal").text(total);
     $("#playerGoal").text(goal);
     $("#winOrLose").text("");
     $("#winner").text("Wins: "+wins);
+    $("#loser").text("Losses: "+ losses)
 
+    //On click events for the 4 buttons
     $("#firstRandom").on("click", function () {
         total += num1;
         $("#playerTotal").text(total);
@@ -43,11 +49,12 @@ $(document).ready(function () {
 
 
 });
-
+//Win lose function to reset the game
 function winLose() {
     if (goal === total) {
         wins++;
         $("#winOrLose").text("You win!!");
+        $("#winner").text("Wins: " + wins);
         total = 0;
         goal = Math.floor(Math.random() * (120 - 19) + 19);
         num1 = Math.floor(Math.random() * 13);
@@ -62,6 +69,7 @@ function winLose() {
     else if(total>goal){
         losses++;
         $("#winOrLose").text("You lose :(");
+        $("#loser").text("Losses: " + losses);
         total = 0;
         goal = Math.floor(Math.random() * (120 - 19) + 19);
         num1 = Math.floor(Math.random() * 13);
